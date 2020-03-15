7648eda13084c17cc217ecd411af8ffa5959c1b5
camoltoni <cesarmoltoni@gmail.com>
2020-03-15 12:14:33 -0300

    Revert "0.1.0 commit"
    This reverts commit ae05eb95a01bd01110d62493c12c23556743d385.

--- src/pages/about.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import Header from "../components/header"
-export default () =>(<div style={{color:`teal`}}>
-  <Header headerText="About Gatsby" />
-  <p>Such now. Very React</p>
-</div>)
